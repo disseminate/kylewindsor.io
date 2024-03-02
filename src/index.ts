0c3387d18e6baffd396a6806460f26cabe06d889
Kyle Windsor <kyle@kylewindsor.io>
2024-03-03 02:07:57 +0700

    remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,14 @@
-let mouseLocation: [number, number] = [-100000, -100000];
-let lerpedMouseLocation: [number, number] = [-100000, -100000];
-
 const updateYears = () => {
-  const years = new Date().getFullYear() - 2013;
   const element = document.getElementById('years');
-  if (element) {
-    element.innerText = years.toString();
-  }
-};
 
-const resize = () => {
-  const canvas = document.getElementById('background-canvas');
-
-  if (!canvas) {
-    return;
+  if (!element) {
+    throw new Error('No #years found.');
   }
 
-  canvas.setAttribute('width', window.innerWidth.toString(10));
-  canvas.setAttribute('height', window.innerHeight.toString(10));
+  const years = new Date().getFullYear() - 2013;
+  element.innerText = years.toString();
 };
 
 window.addEventListener('load', () => {
   updateYears();
-  resize();
-});
-
-window.addEventListener('resize', () => {
-  resize();
-});
-
-document.addEventListener('mousemove', (event) => {
-  mouseLocation = [event.clientX, event.clientY];
-
-  if (lerpedMouseLocation[0] < 0) {
-    lerpedMouseLocation[0] = mouseLocation[0];
-    lerpedMouseLocation[1] = mouseLocation[1];
-  }
 });
